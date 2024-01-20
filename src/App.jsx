42a9c3bb67bfff0e662ac641f49b8b2f7d4fb6ae
Shivamdhakre80 <shivamdhakre54@gmail.com>
2024-01-20 23:21:45 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import kgButton from "./kgbutton";
+
+function App(){
+return <div>
+    <h1>SHIVA IS HERE</h1>
+<kgButton></kgButton>
+    </div>
+}
+
+export default App; 
---
+++ src/kgbutton.jsx
@@ -0,0 +1,6 @@
+function kgButton(){
+
+    return <button>Like this video</button>
+}
+
+export default kgButton
